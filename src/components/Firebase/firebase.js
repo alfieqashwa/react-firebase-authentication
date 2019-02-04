f9b01d1061bc273fe6d1382e486e88c30cc971db
alfieqashwa <alfieqashwa@gmail.com>
2019-02-04 17:04:10 +0700

    Config Firebase database

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 
 // Initialize Firebase
 const config = {
@@ -16,6 +17,7 @@ class Firebase {
     app.initializeApp(config);
 
     this.auth = app.auth();
+    this.db = app.database();
   }
 
   // *** Auth API ***
@@ -31,5 +33,10 @@ class Firebase {
   doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
 
   doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+  // *** User API ***
+
+  user = uid => this.db.ref(`users/${uid}`);
+  users = () => this.db.ref('users');
 }
 export default Firebase;
